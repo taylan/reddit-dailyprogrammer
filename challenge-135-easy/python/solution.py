07bef20a721c261621291be5fc9487b157da38b2
Taylan Aydinli <taylanaydinli@gmail.com>
2013-09-18 16:28:14 +0300

    Revised and shortened Python solution to Challenge 135 [Easy]

--- challenge-135-easy/python/solution.py
+++ challenge-135-easy/python/solution.py
@@ -7,37 +7,23 @@ def get_operation(a, b):
     nums = [str(random.randint(a, b)) for i in list(range(4))]
 
     tups = zip_longest(nums, ops)
-    return " ".join([item for tup in tups for item in tup if item is not None]) #interleave ops and nums, then extract items from list of tuples
+    return " ".join([item for tup in tups for item in tup if item is not None]) #interleave ops and nums, and extract items from list of tuples
 
 def main():
     while True:
         print("> Enter two integers: ")
         inp = input()
         if inp.lower() == 'q':
-            print("exiting. bye!")
             break
-        try:
-            params = [int(i) for i in inp.split(' ')]
-            print(params)
-            if len(params) != 2:
-                raise Exception
-        except:
-            print("You need to enter two integers")
-            continue
 
-        op = get_operation(*params)
+        op = get_operation(*[int(i) for i in inp.split(' ')][:2])
         print("> op: {0}".format(op))
-        print("> enter result: ")
         user_result = input()
         if user_result.lower() == 'q':
-            print("exiting. bye!")
             break
 
-        try:
-            user_result_is_correct = eval(op) == int(user_result)
-            print("correct!" if user_result_is_correct else "incorrect...")
-        except:
-            print("> try again!")
+        user_result_is_correct = eval(op) == int(user_result)
+        print("correct!" if user_result_is_correct else "incorrect...")
 
 
 if __name__ == "__main__":
