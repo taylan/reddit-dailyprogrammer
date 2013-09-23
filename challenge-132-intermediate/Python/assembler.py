baae3040bd72d46193a39a358aacae10060fce51
Taylan Aydinli <taylanaydinli@gmail.com>
2013-09-23 17:18:33 +0300

    Initial implementation for compiler
    Generic implementation for the compiler. Only have definition for AND
    operand for now. Will add more and test.

--- challenge-132-intermediate/Python/assembler.py
+++ challenge-132-intermediate/Python/assembler.py
@@ -1,42 +1,45 @@
-
+import re
 
 class Assembler(object):
     instructions = {
-        'and': {
-            'variants': [
-                {
-                    'byte_code': '0x00',
-                    'format': '[register] [register]'
-                },
-                {
-                    'byte_code': '0x01',
-                    'format': '[register] variable'
-                }
-            ]
-        }
+        'and': {'[a] [a]': '0x00', '[a] a': '0x01'}
     }
 
     output = []
 
-    def compile_line(self, line):
+    def parse_instruction_params(self, line_num, instruction, arguments):
+        args = re.sub('\d+', 'a', arguments)
+        args = re.sub('\s{2,}', ' ', args)
+        try:
+            variant = instruction[args]
+        except KeyError:
+            raise Exception("Invalid arguments for instruction on line {0}.".format(line_num))
+
+        return "{0} {1}".format(variant, ' '.join(['{0:#04x}'.format(int(x)) for x in re.findall('\d+', arguments)]))
+
+        pass
+
+    def compile_line(self, line_num, line):
         line = line.strip()
         if line == '':
-            pass
+            return
 
         tokens = line.split(' ')
         try:
             instruction = self.instructions[tokens[0].lower()]
         except KeyError:
-            raise Exception("Unrecognized instruction {0}".format(tokens[0]))
+            raise Exception("Unrecognized instruction '{0}' on line {1}.".format(tokens[0], line_num))
 
-        print(instruction)
+        compiled_line = self.parse_instruction_params(line_num, instruction, ' '.join(tokens[1:]))
+        print(compiled_line)
+        #print(instruction)
 
         pass
 
     def compile(self, lines):
         try:
-            for line in lines:
-                self.compile_line(line)
+            for line_num, line in enumerate(lines):
+                self.compile_line(line_num, line)
         except Exception as e:
             print("Compilation error.", e)
 
