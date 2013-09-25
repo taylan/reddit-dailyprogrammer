4775c7131e073de1d535a4b4f1bebcf7c55e2251
Taylan Aydinli <taylanaydinli@gmail.com>
2013-09-25 17:23:36 +0300

    Refactored Python solution to Challenge 128 [Intermediate]

--- challenge-128-intermediate/Python/solution.py
+++ challenge-128-intermediate/Python/solution.py
@@ -10,18 +10,14 @@ def all_roads_repaired(grid):
 
 
 def repair_column(grid, col):
-    print('repairing col ', col)
+    print("Column {0} repaired.".format(col))
     for col, row in [(col, x) for x in range(len(grid))]:
         grid[row][col] = 'x'
     pass
 
 
-def print_grid(grid):
-    [print(' '.join(line)) for line in grid]
-
-
 def repair_row(grid, row):
-    print('repairing row ', row)
+    print("Row {0} repaired.".format(row))
     for col, row in [(x, row) for x in range(len(grid))]:
         grid[row][col] = 'x'
     pass
@@ -30,28 +26,27 @@ def repair_row(grid, row):
 def main():
     with open("input.txt") as input_file:
         grid = initialize_grid(input_file.read().splitlines()[1:])
+    col_grid = list(zip(*grid))
 
     if all_roads_repaired(grid):
-        print('no broken roads')
+        print('No broken roads.')
         exit(0)
-
-    col_grid = list(zip(*grid))
 
     to_repair = dict()
 
     for row_num, row in enumerate(grid):
-        to_repair['r' + str(row_num)] = len([cell for cell in row if cell == '0' or cell[0] == '-'])
+        to_repair['r' + str(row_num)] = len([cell for cell in row if cell == '0' or cell.startswith('-')])
         for col_num, col in enumerate(row):
-            to_repair['c' + str(col_num)] = len([cell for cell in col_grid[col_num] if cell == '0' or cell[0] == '-'])
+            to_repair['c' + str(col_num)] = len([cell for cell in col_grid[col_num] if cell == '0' or cell.startswith('-')])
 
-    to_repair = [x[0] for x in sorted(to_repair.items(), key=lambda x: x[1], reverse=True)]
-    for thingy in to_repair:
+    for thingy in sorted(to_repair, key=to_repair.get, reverse=True):
         func = repair_column if thingy.startswith('c') else repair_row
         func(grid, int(thingy[1]))
         if all_roads_repaired(grid):
             break
 
-    print_grid(grid)
+    print()
+    [print(' '.join(line)) for line in grid]
 
 if __name__ == "__main__":
     main()
