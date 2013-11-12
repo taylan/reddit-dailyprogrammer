8d57ec23dc28e709d2be217b37d3a760ce9de6b2
Taylan Aydinli <taylanaydinli@gmail.com>
2013-11-12 10:58:39 +0200

    Added Python solution for Challenge 140 (Easy)

---
+++ challenge-140-easy/python/solution.py
@@ -0,0 +1,15 @@
+from string import capwords
+
+
+# 0 > lower
+# 1 > capwords
+# 2 >
+
+def main():
+    ops = [lambda x: capwords(x), lambda x: x.lower(), lambda x: x.upper()]
+    for p in [l.split(' ') for l in open('input.txt').read().splitlines()]:
+        print(('' if p[0] == '0' else '_').join(list(map(ops[int(p[0])], p[1:]))))
+
+
+if __name__ == '__main__':
+    main()
