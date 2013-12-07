f625598299b1708d393e4a327c84629e61a54783
Taylan Aydinli <taylanaydinli@gmail.com>
2013-12-07 20:23:37 +0200

    Fixed code formatting for challenge 143

--- challenge-143-easy/python/solution.py
+++ challenge-143-easy/python/solution.py
@@ -1,8 +1,4 @@
-def main():
-    braille_dict = dict([l.split(' ') for l in open('dict.txt').read().splitlines()])
-    print(''.join(map(braille_dict.get, map(lambda x: ''.join(x), zip(
-        *[l.split(' ') for l in open('input.txt').read().splitlines()])))))
 
-
-if __name__ == '__main__':
-    main()
+braille_dict = dict([l.split(' ') for l in open('dict.txt').read().splitlines()])
+print(''.join(map(braille_dict.get, map(lambda x: ''.join(x), zip(
+    *[l.split(' ') for l in open('input.txt').read().splitlines()])))))
