92c93f41532d3002f2a5c5109956e358309f8a7e
Taylan Aydinli <taylanaydinli@gmail.com>
2013-12-12 14:09:03 +0200

    Improved solution for challenge 144 easy

--- challenge-144-easy/python/solution.py
+++ challenge-144-easy/python/solution.py
@@ -1,11 +1,13 @@
-from itertools import groupby
 from operator import itemgetter
 
 
 def main():
-    sub = lambda x, y: x - y
-    prods = sorted([(l.split(' ')[0], int(l.split(' ')[1])) for l in open('input.txt').read().splitlines()], key=itemgetter(0))
-    [print(k, v) for k, v in [(k, sub(*map(itemgetter(1), g))) for k, g in groupby(prods, itemgetter(0))] if v != 0]
+    sub = lambda x, y: int(x) - int(y)
+    sort = lambda l: sorted(l, key=itemgetter(0))
+    lines = [l.split(' ') for l in open('input.txt').read().splitlines()]
+    changes = [(k[0][0], sub(*map(itemgetter(1), k))) for k in
+             (zip(sort(lines[:len(lines) // 2]), sort(lines[len(lines) // 2:])))]
+    [print(prod, difference) for prod, difference in changes if difference != 0]
 
 
 if __name__ == '__main__':
